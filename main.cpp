f4d240f22fb50f89fd8ddf2eae355771dfb35ada
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-11-25 11:48:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-   int i,n,primo=1,secondo=1,terzo;
-   do{
-      cin>>n;
-      if(n<2)
-         cout<<"error"<<endl;
-   }while(n<2);
-   cout<<primo<<endl;
-   cout<<secondo<<endl;
-   for(i=2;i<n;i++){
-      terzo=primo+secondo;
-      primo=secondo;
-      secondo=terzo;
-      cout<<""<<terzo<<endl;
-      }      
+   int n=0,a=1,b=1,c=0;
+   cin>>n;
+      if(n>=2){
+         cout<<a<<endl<<b<<endl;
+         for(inti=2;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout<<c<<endl;
+         }
+      }
+   else{
+      cout<<"error"<<endl;
+   }
    return 0;
 }
