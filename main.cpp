4d2412c01f958e5ca957f25a470c6f425f996ab6
Ceciutz <115075907+Ceciutz@users.noreply.github.com>
2022-11-25 11:58:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main() {
-   int n=0,a=1,b=1,c=0;
+   int n=0
+   int f=1,f1=1;
    cin>>n;
       if(n>=2){
-         cout<<a<<endl<<b<<endl;
-         for(int i =2;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-            cout<<c<<endl;
+         cout<<f<<endl
+         cout<<b<<endl;
+         for(int i =1;i<n-1;i++){
+            int temp=f;
+            f+=f1;
+            f1=temp;
+            cout<<f<<endl;
          }
-      }
-      else{
+      }else{
        cout<<"error"<<endl;
    }
    return 0;
